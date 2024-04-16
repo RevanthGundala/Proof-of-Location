43543762ee09a67f00b77cf3494ae6e84addaf96
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-04-16 12:28:46 -0700

    modified ui

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,6 +1,29 @@
+"use client";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+export const config = getDefaultConfig({
+  appName: "Proof of Location",
+  projectId: "ac35aa265518e49cf52e0497c2efeba2",
+  chains: [sepolia, mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
 
 import { cn } from "@/lib/utils";
 
@@ -19,8 +42,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontSans.variable
         )}
       >
-        <main>{children}</main>
-        <Toaster />
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <main>{children}</main>
+              <Toaster />
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
       </body>
     </html>
   );
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,22 +1,62 @@
+"use client";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { getAccount } from "@wagmi/core";
+import { config } from "./layout";
 
 export default function Home() {
+  const { status } = getAccount(config);
+
+  // TODO: Switch to backend
+  async function getIPAddress() {
+    try {
+      console.log("Fetching IP Address");
+      const res = await fetch("https://api.ipify.org/?format=json");
+      const data = await res.json();
+      console.log(data);
+      return data.ip;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function prove(e: any) {
+    e.preventDefault();
+    try {
+      console.log("hi");
+      // const res = await fetch("http://localhost:8000/api/prove");
+      // const data = await res.json();
+      // console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <Button
-      variant="outline"
-      onClick={() =>
-        toast("Event has been created", {
-          description: "Sunday, December 03, 2023 at 9:00 AM",
-          action: {
-            label: "Undo",
-            onClick: () => console.log("Undo"),
-          },
-        })
-      }
-    >
-      Show Toast
-    </Button>
+    <>
+      <div className="flex flex-row justify-center items-center min-h-screen">
+        {status === "disconnected" ? (
+          <ConnectButton />
+        ) : (
+          <Button
+            variant="outline"
+            onClick={async (e) => {
+              await prove(e);
+              toast("Proof has been created", {
+                description: "Sunday, December 03, 2023 at 9:00 AM",
+                action: {
+                  label: "Close",
+                  onClick: () => console.log("Closed toast"),
+                },
+              });
+            }}
+          >
+            Access and Prove location
+          </Button>
+        )}
+      </div>
+    </>
   );
 }
